7a810a335a5578e9f7f94c4a910ecf7ec0f3e178
Keo Dara <lykeosovandara@gmail.com>
2024-11-18 21:52:20 +0700

    update ggg

--- lib/cores/game_config.dart
+++ lib/cores/game_config.dart
@@ -3,6 +3,8 @@ class GameConfig {
 
   static const musicBgValum = 0.05;
 
-  static const rewardAdUnit = "ca-app-pub-3893128472916644~5116894682";
+  // static const rewardAdUnit = "ca-app-pub-3893128472916644~5116894682";
   static const testrewardAdUnit = "ca-app-pub-3940256099942544/5224354917";
+  static const rewardAdUnit = testrewardAdUnit;
+  // static const testrewardAdUnit = "ca-app-pub-3940256099942544/5224354917";
 }
